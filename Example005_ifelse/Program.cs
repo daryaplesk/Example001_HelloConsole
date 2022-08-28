a182a7683c1e6e49a826da9cc36d8957a4342746
Darya <daryaplesk@gmail.com>
2022-08-28 16:36:02 +0300

    Project ifelse created

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "денис")
+{
+    Console.WriteLine("Ура, это же Дениска-пиписка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

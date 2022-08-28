e6ae08894c28f87fc3f1608b43dbc09643cf1ae4
Darya <daryaplesk@gmail.com>
2022-08-28 16:13:57 +0300

    Random numbers added

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 7;
-int numberB = 3;
+﻿int numberA = new Random().Next(1, 10); // 0....9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
